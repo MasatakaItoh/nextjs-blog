110b8eb96d2e0e1997efb89ae19d8a3aad9fcb2d
FLAT 伊藤 <ito@wd-flat.com>
2022-02-01 01:43:13 +0900

    [add] Headerコンポーネントを作成

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,5 +1,22 @@
 import { memo, VFC } from 'react';
+import Link from 'next/link';
 
 export const Header: VFC = memo(() => {
-  return <header>headerです</header>;
+  return (
+    <header>
+      <p>
+        <Link href='/'>Next.js Blog</Link>
+      </p>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/articles/'>記事一覧</Link>
+          </li>
+          <li>
+            <Link href='/articles/'>記事一覧</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 });
