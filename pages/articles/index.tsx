9550e4431b93b1ed7d5a759a9ab1dc0eb7a663f5
FLAT 伊藤 <ito@wd-flat.com>
2022-02-01 01:50:08 +0900

    [add] 記事一覧ページを作成

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,22 +1,22 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 
-import { Meta } from '../components/common/Meta';
-import { client } from '../libs/client';
-import { Article } from '../types/api/article';
+import { Meta } from '../../components/common/Meta';
+import { client } from '../../libs/client';
+import { Article } from '../../types/api/article';
 
 type Props = {
   articles: Array<Article>;
 };
 
-const Home: NextPage<Props> = (props) => {
+const Articles: NextPage<Props> = (props) => {
   const { articles } = props;
 
   return (
     <>
       <Meta />
       <main>
-        <h1>Nuxt.js Blog</h1>
+        <h1>記事一覧</h1>
         <ul>
           {articles.map((article) => (
             <li key={article.id}>
@@ -26,13 +26,12 @@ const Home: NextPage<Props> = (props) => {
             </li>
           ))}
         </ul>
-        <Link href='/articles/'>記事一覧を見る</Link>
       </main>
     </>
   );
 };
 
-export default Home;
+export default Articles;
 
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: 'article' });
