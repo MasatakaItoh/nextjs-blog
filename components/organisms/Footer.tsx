fe854d77560396de021a3d735803ac60e588a01e
FLAT 伊藤 <ito@wd-flat.com>
2022-02-01 01:45:35 +0900

    [add] Footerコンポーネントを作成

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,5 +1,22 @@
 import { memo, VFC } from 'react';
+import Link from 'next/link';
 
 export const Footer: VFC = memo(() => {
-  return <footer>footerです</footer>;
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'>TOP</Link>
+          </li>
+          <li>
+            <Link href='/articles/'>記事一覧</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <small>&copy; Masataka Ito</small>
+      </div>
+    </footer>
+  );
 });
